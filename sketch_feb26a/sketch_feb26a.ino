e74be6c517ba0f0b83c2970cf45b70862a20b9dc
Ben <bv.lafrance@gmail.com>
2023-03-24 09:51:16 -0400

    Update sketch_feb26a.ino
    code cleanup and logic checks

--- sketch_feb26a/sketch_feb26a.ino
+++ sketch_feb26a/sketch_feb26a.ino
@@ -15,7 +15,7 @@ float chlore_necessaire;
 int fillWarn = 15;                                  //variable pour définir le pourcentage dont la lumière du niveau réservoir s'allume , un pourcentage (15%)
 int dispenseDelay = 3600000;                        //1hr delay
 int poolVolume = 14366;                             //volume de la piscine en US gallon
-int pumpRate = 0.160;                               //débit de la pompe
+int pumpRate = 0.160;                               //débit de la pompe (L/sec)
 int alerte_nbr_jours = 5;                           //nombre de jours que la cliente veut etre alerté avant que le réservoir est vide
 int reservoirFull = 19;                             //volume du reservoir quand il est remplit, (L)
 float dailyAV = 0.66666666666;                      //calcul aprox. de L de chlore utilisé par jours
@@ -36,10 +36,12 @@ float getCapteurPpm()                               //maybe PPM, maybe PH, TBD**
 // fonction pour calculer le chlore necessaire pour augmenter le ppm de le montant necessaire
 float getChloreNecessaire()
 {
-  //faire le calcule en prennant la valuer de PH donnée par getCapteurPpm(), retourne une valeur en Oz  
-  //**********on assume sensor PH, et non PPM
-  int PH = getCapteurPpm();
-  return (7.5 - PH)*(poolVolume)*0.002;
+  //faire le calcule en prennant la valuer de PH donnée par getCapteurPpm(), retourne une valeur en Oz  **********on assume sensor PH, et non PPM
+  
+    //int PH = getCapteurPpm();
+    int PH = 6; //for test
+    
+    return (7.5 - PH)*(poolVolume)*0.002;
 }
 
 
@@ -67,8 +69,6 @@ void allumerPompe(int timeX)
 }
 
 
-//Nouveau code pour la fonction loop()
-bool ensemble = false;
 
 void loop(int pourcentage) {
   
@@ -76,46 +76,27 @@ void loop(int pourcentage) {
   
   while (true) {
     
-    chlore_necessaire = getChloreNecessaire()*0.0295735;//conversion from Oz to L
-    
-      
-      
-      
-    if (chlore_necessaire < 0.2) {
+    chlore_necessaire = getChloreNecessaire()*0.0295735;     //calculate how much chlorine to pump, and conversion from Oz to L
+//******************************************************************************************************************************************      
+    if (chlore_necessaire < 0.2) { //if the necessary injection amount is below 200mL, wait
 
       delay(dispenseDelay);                             // delai de une heure si le montant à ajouter est moins de 200mL
-
-      ensemble = true;
       break;
-
     }   
       
-      
-      
-    
-    if (chlore_necessaire > 0.2){
+//******************************************************************************************************************************************    
+    if (chlore_necessaire > 0.2){ //if the necessary injection amount is above 200mL
 
       float chloreRestant =pourcentage1*(reservoirFull)/100;
       
-        
-        
-      if (chloreRestant > chlore_necessaire){ 
+      if (chloreRestant > chlore_necessaire){           //check si 
                
-        int daysLeft = (chloreRestant/dailyAV) - 1;   //dailyAV to be more accurately defined, algo to calculate how many days worth of chlorine left
-          
-        if(daysLeft<=alerte_nbr_jours){
-          allumerLumiere(pourcentage1);
-        }
-        
         int time = chlore_necessaire*1000/pumpRate;   //calculate how long pump turns on (ms)
-         
         allumerPompe(time);                           //active la pompe
       } 
-      
-    }
-       
-                          // le break est important ici
-      break;
+    }  
+//******************************************************************************************************************************************
+    break;                                            // le break est important ici
   }
 }
 
@@ -125,8 +106,8 @@ int main() {
     
     //*************code pour niveau de reservoir******************
     pinMode(7,OUTPUT);
-    Serial.begin(115200);                       // Open serial monitor at 115200 baud to see ping results.
-    delay(50);                                  // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+    Serial.begin(115200);                               // Open serial monitor at 115200 baud to see ping results.
+    delay(50);                                          // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
     float distance = sonar.ping_cm();                   // Send ping, get distance in cm and print result (0 = outside set distance range), distance entre detecteur et chlore
     float niveau = hauteur-distance;                    //cm de chlore dans le reservoir
     float pourcentage = (niveau/hauteur)*100;           //pourcentage de chlore dans reservoir
@@ -135,8 +116,6 @@ int main() {
     allumerLumiere(pourcentage); // checks reservoir level to see if the fill warning light should be turned on 
       
     if(pourcentage > 5){                                //if there is enough chlorine left in reservoir for current pump injection
-      chlore_necessaire = getChloreNecessaire();
-      ensemble = false;
       loop(pourcentage);
     } //if there is NOT enough chlorine left in reservoir for current pump injection, system will keep looping in the Do...While and continuously check reservoir until it is filled again
     
